1553cef6d0ddaf8e0df58713fc82e93d35b5788a
Amiel Martin <amiel.martin@gmail.com>
2013-06-13 10:23:36 -0700

    Simple failing example of afmotion

--- Rakefile
+++ Rakefile
@@ -2,7 +2,13 @@
 $:.unshift("/Library/RubyMotion/lib")
 require 'motion/project/template/ios'
 
+require 'afmotion'
+
 Motion::Project::App.setup do |app|
   # Use `rake config' to see complete project settings.
   app.name = 'afnetworkingtest'
+
+  app.pods do
+    pod 'AFNetworking'
+  end
 end
--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -1,5 +1,11 @@
 class AppDelegate
   def application(application, didFinishLaunchingWithOptions:launchOptions)
+    AFMotion::HTTP.post("http://localhost:9292/hello") do |result|
+      alert = UIAlertView.new
+      alert.message = 'Network request finished'
+      alert.show
+    end
+
     true
   end
 end
